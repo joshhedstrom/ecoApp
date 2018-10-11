fb9060c99dac0f25455718c707591a082886c76b
--v <josh.hedstrom@gmail.com>
2018-10-11 09:51:08 -0600

    debugged server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
 // const routes = require('./routes');
-// const path = require('path');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const morgan = require('morgan');
@@ -10,7 +10,7 @@ const morgan = require('morgan');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(routes);
+// app.use(routes);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
