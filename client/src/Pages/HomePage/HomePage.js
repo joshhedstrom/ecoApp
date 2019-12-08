e177bb6039e6be3c7d0c3dda90f25cd65c99e385
Josh Hedstrom <josh.hedstrom@gmail.com>
2019-12-07 20:46:08 -0700

    added header and scaffolded cards

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
+import './HomePage.css';
+import solar1 from  '../../assets/solar1.jpg'
+import windmill1 from '../../assets/windmill1.jpg'; 
+import notebook from '../../assets/notebook.jpg'; 
+
+import HeaderComponent from '../../Components/HeaderComponent';
+import CardComponent from '../../Components/CardComponent';
+
 class HomePage extends Component {
-  state = {};
+
+  state = {}
 
   render() {
-    return(
-        <div>
-            <h1>this is the homepage</h1>
+    return (
+      <div>
+        <div className="header__image">
+          <HeaderComponent />
+          </div>
+          <div className="cards__wrapper">
+            <CardComponent title="Clean Energy" image={solar1} />
+            <CardComponent title="Recycling" image={windmill1} />
+            <CardComponent title="Blog" image={notebook} />
         </div>
+      </div>
     );
   }
 }
