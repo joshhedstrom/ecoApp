3d0c0a0a4db4145625cbec9830e31afdef307fdd
--v <josh.hedstrom@ruralsourcing.com>
2019-12-24 07:37:04 -0700

    updated routes to reflect pages layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
-import LoginPage from './Pages/LoginPage';
-import SignupPage from './Pages/SignupPage';
 
 class App extends Component {
   render() {
@@ -11,9 +9,12 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/" component={HomePage} />
-            <Route exact path="/articles" component={LoginPage} />
-            <Route exact path="/blog" component={SignupPage} />
-            <Route exact path="/products" component={SignupPage} />
+            <Route exact path="/articles" component={HomePage} />
+            <Route exact path="/blog" component={HomePage} />
+            <Route exact path="/products" component={HomePage} />
+            <Route exact path="/petitions" component={HomePage} />
+            <Route exact path="/partnerships" component={HomePage} />
+            <Route exact path="/funding" component={HomePage} />
           </Switch>
         </div>
       </Router>
