0afd59360d6ac656fae7033d01bdbe36c5013d17
--v <josh.hedstrom@protonmail.com>
2019-12-30 15:06:02 -0700

    made homepage mobile responsive

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 
-import './index.css';
-
-import menuItems from '../menuItems.json'
-
 import HeaderComponent from '../../components/HeaderComponent';
 import CardComponent from '../../components/CardComponent';
 
+import menuItems from '../menuItems.json';
+import './index.css';
+
 class HomePage extends Component {
-
-  state = {}
-
+  state = {};
 
   render() {
     return (
-      <div>
+      <div className="homepage__wrapper">
         <div className="header__image">
           <HeaderComponent />
-          </div>
-          <div className="cards__wrapper">
-            {menuItems.menuItems.map((item, i) => <CardComponent key={item.title} title={item.title} img={item.img} {...item} />)}
+        </div>
+        <div className="cards__wrapper">
+          {menuItems.menuItems.map((item, i) => (
+            <CardComponent key={item.title} {...item} />
+          ))}
         </div>
       </div>
     );
